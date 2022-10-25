d98589459787d7cfe295c0f5435bca1a4eefce90
Brad Simpson <bradsimpson@icloud.com>
2022-10-25 09:53:05 -0400

    updated for w17d1 july cohort

--- week_17/D1/lecture-code/w17d1.py
+++ week_17/D1/lecture-code/w17d1.py
@@ -1,59 +1,75 @@
-# FUNCTIONS INTRO
+# FUNCTIONS
 
-def get_average(num1, num2):
-    """This function will accept two
-    numbers and return their average """
-    average = (num1 + num2) / 2
-    print("the average is:", average)
+def get_average(num_1, num_2):
+    """ This fucntion will accept 2 
+    numbers and print 
+    their average    
+    """
+    average = (num_1 + num_2) / 2
+    print("Inside function", average)
     return average
 
-# ran_average = get_average(4, 6)
-# print(ran_average)
 
-# STRING STUFF
+# ran_average = get_average(4, 2)
+# print("Outside", ran_average)
+# help(get_average)
 
-lunch = "pizza"
+long_string = '''This is some
+long long
+multiline text '''
+# print(long_string)
+
+
+a = 4
+n = 236
+x = "Pizza"
+
+# STRINGS
+
+lunch = "Pizza"
+# print(lunch)
+# lunch = 1
+# print(lunch)
+# LUNCH = 2
+# print(LUNCH)
+# LUNCH = "Sandwich"
+# print(LUNCH)
+
 # print(lunch)
 # print(len(lunch))
+
 
 # print("Pizza")
 # print('Pizza')
 # print("Let's")
-# print(""" This is a 
-# multi line string filled 
-# with lots of great stringy stuff """)
-
-# print(''' This is a multi line string \n filled 
-# with lots 
-# of great stringy stuff ''')
-
+# print('Let\'s')
+# print(""" THis is a 
+# multi
+# multi
+# multi
+# very 
+# long string
+# """)
 
 a = 1
-b = 2
-# print(a)
-# a = 'Pizza'
-# print(a)
+b = 5
 
-# LUNCH = "Pizza"
+# print(f'Let\'s add {a} and {b} together to get {a + b}')
+# print("Let's add {} and {} together to get {}".format(a, b, a + b))
+# print("Let's add {thing1} and {thing2} together to get {thing3}".format(thing3=a + b, thing2=b, thing1=a))
 
-# print(f"Lets add {a} and {b} together to get {a + b} ")
-# print("Lets add {} and {} together to get {} ".format(a, b, a + b))
-# print("Lets add {thing1} and {thing2} together to get {thing3} ".format(thing3=a+b, thing1=a, thing2=b))
+# print("Pizza " + "is " + "delicious!" )
+# print("Pizza " * 4)
 
+sentence = "Brad is very much obsessed with pizza today!"
 
-# print("Pizza" + " is" + ' the best')
-# print("pizza " * 4)
+# print(sentence[::-1])
+# sentence[start:stop:step] # stop is not inclusive
+# print(sentence.split())
+# print(list(sentence))
 
-sentence = "Brad is very much obsessed with pizza today"
-
-# print(sentence[2])
-# print(sentence[1:4])
-# print(sentence[:-5])
-# print(sentence[1:8:2])
-# print(len(sentence))
-
-names = ["Brad", "John", "Cesar", 'David'] 
-join_str = ' and '
+# names = ['Brad', 'John', 'David', 'Cesar']
+# join_str = ' and '
 # print(join_str.join(names))
 
 
@@ -62,46 +78,76 @@ join_str = ' and '
 a = True
 b = False
 
-# && ||  !
+# && || !
 # and or not 
-# print(True and False) 
+
+# print(True and False)
 # print(True or False)
-# print(False and True)
-# print(not False)
+# print(not True)
 # print(True + 1)
 # print(False + 1)
-# print("pizza" + 1)
+# print(1 == 1)
+# print(1 == 2)
+# print(1 != 1)
+# print(2 != 1)
+# print(int("1") == 1)
 
-# NUMBER STUFF
 
-results = 2 * 4
+# NUMBERS
+
+results = 2 * 4.0
 # print(results)
-# print(int('1234'))
-# print(float("1235.23"))
+# print(int("1234"))
+# print(float("1234.56"))
 # print(float(123))
 
-# data = int(input("Give me some input"))
-# print(data)
 
 # + - * / % // **
-# 
-# print(4**2)  
-# print(4/2)
-# print(4//2)
+# print(2**4)
 
-def long_division(num1, num2):
-    """ function that takes in two values and return the result, and remainder"""
-    result = num1 // num2
-    remainder = num1 % num2
+def long_division(num_1, num_2):
+    result = num_1 // num_2
+    remainder = num_1 % num_2
     print(result, remainder)
-    return f'{num1} divided by {num2} equals {result} reaminder {remainder}'
+    return f"{num_1} divided by {num_2} is {result} remainder {remainder}"
 
 
-# print(long_division(25, 5))
+# print(long_division(25, 3))
+
+num = 20
+num += 1
+
+my_float = 123.45678
+# print(round(my_float, 4))
+
+
+dinner = ['Salad', 'Pizza', 'wings']
+dinner = "salad"
+
+num = 30
+num += 5
+
+bags = 50
+cases = 50
+
+# print(bags)
+# print(cases)
+
+DESSERT = 'ice cream'
+
+num = 1
+
+def new_func():
+    num = 1
+    if true:
+        num = 2
+    for food in dinner:
+        num = 3
+
 
 
 # IDENTITY vs EQUALITY
-#    is    vs    ==
+#    is         ==
 
 my_int = 4
 my_float = 4.0
@@ -112,18 +158,17 @@ my_float = 4.0
 # print("2" == '2')
 # print("2" is '2')
 
-
 # a = None
 # print(id(a))
 # b = 'pizza'
 # print(id(b))
+# print(a is b)
 # c = None
 # print(id(c))
-# print( a is c)
-# d = "pizza"
+# print(a is c)
+# d = 'pizza'
 # print(id(d))
-# print(b is d)
+# print(d is b)
 
-print(True == 1)
-print(True is 1)
-print(True != 1)
+# print( True == 1)
+# print( True is 1)
