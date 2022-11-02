0a2948bfdc71654f4542f349d2d8be7b143f18a6
Brad Simpson <bradsimpson@icloud.com>
2022-11-02 14:11:57 -0400

    updated for w18d3 lecture

--- week_18/Day-3/lecture-code/dad_jokes/__init__.py
+++ week_18/Day-3/lecture-code/dad_jokes/__init__.py
@@ -2,37 +2,35 @@ from flask import Flask, render_template, redirect
 from .config import Config
 # from .db_jokes import jokes
 from random import choice
-from .routes.joke_routes import joke_router
-from .routes.user_routes import user_router
+from .routes.joke_routes import jokes_router
+from .routes.user_routes import users_router
 from .models import db, Joke
 
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 db.init_app(app)
 
-app.register_blueprint(joke_router)
-app.register_blueprint(user_router, url_prefix="/users")
+
+app.register_blueprint(jokes_router)
+app.register_blueprint(users_router, url_prefix="/users")
 
 
 
 @app.route('/')
 def index():
     jokes = Joke.query.all()
+    # print(jokes)
     joke = choice(jokes)
-
-    response = joke.to_dict()
-    print(response)
-    return render_template("index.html", joke=joke, title='Dad Jokes')
-    # return response 
-
-
-@app.route("/another")
-def sample_redirect():
-    return "<h1>This is a whole new route!</h1>"
+    print(joke.to_dict())
+    return render_template("index.html", joke=joke)
+    # return redirect("/another", 302)
+    # return joke.to_dict()
 
 
-# @app.route('/all')
-# def all_jokes():
-#     # eventually we will query our DB  jokes = Joke.query.all()
-#     return render_template("all_jokes.html", jokes=jokes)
+@app.route('/another')
+def another_route():
+    return "<h2>Totally different route</h2>"
+
+
--- week_18/Day-3/lecture-code/dad_jokes/config.py
+++ week_18/Day-3/lecture-code/dad_jokes/config.py
@@ -2,6 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY")
-    # eventually store DB connection here too
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
