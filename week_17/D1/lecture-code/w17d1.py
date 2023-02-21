79501f5ff310b79d13e1df3bf96642acfab84fe6
Brad Simpson <bradsimpson@icloud.com>
2023-02-20 19:16:56 -0500

    updated for w17d1

--- week_17/D1/lecture-code/w17d1.py
+++ week_17/D1/lecture-code/w17d1.py
@@ -1,59 +1,71 @@
 # FUNCTIONS
+
+# Convention for a constant variable
+IMPORTANT_VARIABLE = 3.14
+average = "Average"
+
 def get_average(num_1, num_2):
-    """This function will accept two numbers
-    and return the average"""
+    """This function takes in two number 
+    and return their average"""
     average = (num_1 + num_2) / 2
     print(average)
     return average
-    print("Not gonna see this")
+    print("Never gonna see this")
 
-# ran_average = get_average(4,6)
+
+# ran_average = get_average(4, 6)
 # print(ran_average)
+
 # help(get_average)
 
-
-# STRING STUFF
-# lunch = "Pizza"
-dinner = 'Wings'
+# STRINGS
+lunch = "Pizza"
+dinner = 'Hamburger'
 # print(lunch)
-# print(dinner)
-# print(len(lunch))
-# print(""" This is a
-# multi line string
-# and we can do 
-# lots and 
-# lots of lines
-# cool
+
+# print("Let's")
+# print('Brad said, "Python version 3.9.4 is the best one!"')
+# print('let\'s')
+# print("""This is a multi
+# line string
+# it will preserver new lines
+# and such
 # """)
 
-a = 1
-b = 2
-# print(a)
-# a = "Pizza"
-# print(a)
+# a = 1
+# b = 2
+# c = []
+# # print(a)
+# # a = 'Pizza'
+# # print(a)
+# LUNCH = "Pizza"
 
-LUNCH_TIME = "Pizza"
+# print(f"Let's add {a} and {b} together to get {a + b}")
+# print("Let's add {0} and {2} together to get {1}".format(a, a + b, b))
+# print("Let's add {thing1} and {thing3} together to get {thing2}".format(thing1=a, thing2=a + b, thing3=b))
 
-# print(f"Let's add {a} and {b} together to get {a + b} ")
-# print("Let's add {1} and {0} together to get {2}".format(a, b, a+b))
-# print("Let's add {thing2} and {thing1} together to get {thing3}".format(thing2=a, thing1=b, thing3=a+b))
 
-# thing1 = "Taco"
-# print(thing1)
+# print('{:,}'.format(123_456_789))
+# print(123_456_789)
 
-# print("Pizza" + " is" + " so" + ' delicious')
-# print("Pizza is so" + (" very," * 4) + " yummy!" )
+# print("Pizza" + " is" + " great!")
+# print("Pizza! " * 4)
 
-# sentence = "Brad is very much obsessed with Pizza today!"
+sentence = 'Brad is very much obsessed with pizza today!'
 
+# print(sentence[3])
+# print(sentence[-4])
+# print(sentence[start:stop:step])
 # print(sentence[::-1])
-# print(sentence[2:10:1])
-# print(sentence.split("a"))
+# print(sentence.split('a'))
 # print(list(sentence))
 
-# names = ["Brad", "David N.", "Andrew", "David D."]
-# join_str = ', '
-# print(join_str.join(names)) 
+names = ['Brad', 'David', 'Andrew', 'Keegan']
+join_str = ', '
+# print(join_str.join(names))
+
+# print(len(sentence))
+
 
 
 # BOOLEANS
@@ -62,46 +74,58 @@ b = False
 
 # && || !
 # and or not
+
 # print(True and False)
 # print(True or False)
-# # print(not False)
-# # print(True + 1)
-# # print(False + 10)
-# print(1 == 2)
-# print(1 == True)
-# print(1 == 1.0)
-# print(1 != 2)
-
+# print(not True)
+# print(True + 1)
+# print(False + 2)
+# print(True == 1)
+# print(True != 1)
 
 # NUMBERS
-results = 2.0 * 4.0
-# print(results)
-# print(int('1234') + 3)
+# result = 2.0 * 4.0
+# print(result)
+# print(int(123.45))
+# print(float(4))
+# print(int("1234"))
 # print(str(1234))
-# print(float(235))
-# print(int(2367.8))
 
-
-# OPERATIONS
 # + - * / % // **
 # print(4**2)
 
-def long_division(num_1, num_2):
-    results = num_1 // num_2
-    remainder = num_1 % num_2
-    print(results, remainder)
-    return f"{num_1} divided by {num_2} is {results} remainder {remainder}"
+def long_division(dividend, divisor):
+    """Takes in 2 numbers and returns their result and remainder"""
+    quotient = dividend // divisor
+    remainder = dividend % divisor
+    print(quotient, remainder)
+    return f'{dividend} divided by {divisor} is {quotient} remainder {remainder}'
 
 
-# print(long_division(253_456, 3_234))
+# print(long_division(25, 3))
+
+# VARIABLES
+# x = 1
+# num = 40
+# num += 1
+
+# TODAYS_DAY = "Monday"
+
+# TODAYS_DAY = "Wednesday"
+# print(TODAYS_DAY)
+
+# bags = 50
+# cases = 50
+# print(bags)
+# print(cases)
+# print( (1 + 2) * 3)
 
 
-# IDENTITY                      vs          EQUALITY
-# JS: === (identity/type and value)            == (value)
-# PY:  is                                      ==
+# IDENTITY vs EQUALITY
+#    is    vs  ==
 
-# my_int = 4
-# my_float = 4.0
+my_int = 4
+my_float = 4.0
 
 # print(my_float == my_int)
 # print(my_float is my_int)
@@ -109,32 +133,16 @@ def long_division(num_1, num_2):
 # print("2" == '2')
 # print("2" is '2')
 
+
 # a = None
-# print('A',id(a))
-# b = 'pizza'
-# print('B',id(b))
-# c = None
-# print('C',id(c))
-# d = 'pizza'
-# print('D',id(d))
-# print(a is c)
-# print(b is d)
+# print(id(a))
+# b = None 
+# print(id(b))
+# print( a is b )
+# lunch = "pizza"
+# print(id(lunch))
+# dinner = 'pizza'
+# print(id(dinner))
 
-print(True == 1)
-print(True is 1)
-
-bags = cases = boxes = barrels = 50
-
-print(bags, cases)
-
-bags = "Bags"
-print(bags)
-
-num_4 = 40
-num_4 += 1
-
-DESSERT = "ice cream"
-
-DESSERT = "pecan pie"
-
-print(DESSERT)
+val = 1
+print(val is True)
