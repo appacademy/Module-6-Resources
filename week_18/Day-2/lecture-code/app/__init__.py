987770532b3f16cf8845b68acb77f696f9b3294e
Brad Simpson <bradsimpson@icloud.com>
2023-03-28 13:50:31 -0400

    updated for w18d2 lecture

--- week_18/Day-2/lecture-code/app/__init__.py
+++ week_18/Day-2/lecture-code/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect 
+from flask import Flask, render_template , redirect 
 from .config import Config
 from .routes.post_routes import posts
 from .routes.user_routes import users
@@ -6,21 +6,27 @@ from .routes.user_routes import users
 
 app = Flask(__name__)
 
-
 app.config.from_object(Config)
 app.register_blueprint(posts, url_prefix="/posts")
 app.register_blueprint(users, url_prefix="/users")
+
+
+# print("in __init__", __name__)
 
 
 @app.route("/")
 def index():
     """renders the home page"""
     return render_template("index.html")
-    # return redirect("/another", 304)
+    # return redirect("/another")
 
 
 @app.route("/another")
-def another_route():
-    return "<h1>This is an entirely different route!</h1>"
+def another():
+    return "<h2>This is another route!</h2>"
+
+
+
+
 
 
--- week_18/Day-2/lecture-code/app/routes/post_routes.py
+++ week_18/Day-2/lecture-code/app/routes/post_routes.py
@@ -2,45 +2,43 @@ from flask import Blueprint, render_template, redirect
 from ..posts import posts as seed_posts
 from ..forms.post_form import PostForm
 from datetime import date
-from random import randint
+from random import randint 
 
 posts = Blueprint("posts", __name__)
 
-# print("inside the post BP",__name__)
+# print("inside posts BP", __name__)
 
 
 @posts.route("/all")
 def get_all_posts():
-    # Query for all posts
+    """get all posts and display them"""
     # posts = Post.query.all()
+    print(seed_posts)
     sorted_posts = sorted(seed_posts, key=lambda post: post['date'], reverse=True)
     return render_template("feed.html", posts=sorted_posts)
 
 
+
 @posts.route("/<int:id>")
-def get_post_by_id(id):
+def get_post_by_id(id): 
+    """return a single post by its ID"""
     print(id)
-    # Query
-    # post=Post.query.get(id)
-    one_post = [post for post in seed_posts if post['id'] == id]
+    one_post = [post for post in seed_posts if post["id"] == id ]
     print(one_post)
     return render_template("feed.html", posts=one_post)
 
+       
 
+@posts.route("/new", methods=["GET", "POST"])
+def add_new_post():
+    """returns a new post form on get requests, 
+    validates and saves the new resource on post"""
 
-@posts.route("/new", methods=['GET', 'POST'])
-def create_new_post():
-    '''renders an empty form on a get route, validates 
-    the form and creates a new post resource on post 
-    requests
-    '''
     form = PostForm()
-
-
+    # query for data if needed in the form
 
     if form.validate_on_submit():
-        #enter block and create our resource
-        new_post = {
+        new_post ={
             "id": len(seed_posts) + 1,
             "author": form.data["author"],
             "caption": form.data["caption"],
@@ -51,10 +49,11 @@ def create_new_post():
         print(new_post)
         seed_posts.append(new_post)
         return redirect("/posts/all")
+        # return new_post
 
     if form.errors:
         print(form.errors)
         return render_template("post_form.html", form=form, errors=form.errors)
 
-    # form.author.choices = #get from the DB
+
     return render_template("post_form.html", form=form, errors=None)
