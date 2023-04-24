c011fa76f6fe49ee280d31d97ffbe99d61792e1a
Brad Simpson <bradsimpson@icloud.com>
2023-04-24 17:35:32 -0400

    updated for w18d1

--- week_18/Day-1/lecture-code/app/__init__.py
+++ week_18/Day-1/lecture-code/app/__init__.py
@@ -3,36 +3,34 @@ from .config import Config
 from .posts import posts
 
 
-app = Flask(__name__)
 
+app = Flask(__name__)
+print(__name__)
 app.config.from_object(Config)
 
-print(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def index():
-    """renders the home page"""
+    """renders the site home page"""
+    # Queries would go here
+    # and data manipulation you want to do to that query
     return render_template("index.html")
-
 
 
 @app.route("/all")
 def get_all_posts():
-    """get all posts and display them"""
+    """route to fetch and display all posts"""
+    # Query our DB to get all posts
     # posts = Post.query.all()
     return render_template("feed.html", posts=posts)
 
 
-
-@app.route("/post/<int:id>")
-def get_post_by_id(id): 
-    """return a single post by its ID"""
-    print(id)
+@app.route("/<int:id>")
+def get_post_by_id(id):
+    """returns a single post by the given route param id"""
+    # post = Post.query.get(id)
     one_post = [post for post in posts if post["id"] == id ]
-    print(one_post)
     return render_template("feed.html", posts=one_post)
-       
-
 
 
