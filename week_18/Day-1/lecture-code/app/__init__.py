0b436e34305cc72b9ab40d8b40b8f1822243c101
Brad Simpson <bradsimpson@icloud.com>
2023-06-19 17:33:38 -0400

    updated for w18d1

--- week_18/Day-1/lecture-code/app/__init__.py
+++ week_18/Day-1/lecture-code/app/__init__.py
@@ -1,39 +1,32 @@
-from flask import Flask, render_template  
+from flask import Flask, render_template 
 from .config import Config
 from .posts import posts
 
 
-
 app = Flask(__name__)
 print(__name__)
-
 app.config.from_object(Config)
-
 
 
 @app.route("/")
 def index():
-    """Landing page"""
-    # Query the DB
-    # return "<h1>Welcome to our flask server!</h1>"
-    return render_template("index.html", title="Patchstagram")
-
+    """Landing page for our Patchstagram app"""
+    # query for data
+    # any other python logic we want
+    # return render_template("index.html")
+    return {"key": "some values"}
 
 
 @app.route("/all")
-def feed():
-    """Get all post and send to template for display"""
-    # Query for all posts
-    # all_posts = Post.query.all()
-    return render_template("feed.html", posts = posts)
-
+def get_all_posts():
+    """Query for all posts and render a template displaying them"""
+    # posts = Post.query.all()
+    return render_template("feed.html", posts=posts)
 
 
 @app.route("/<int:id>")
 def get_post_by_id(id):
-    """returns a single post by its id"""
-    # onme_post = Post.query.get(id)
-    print(id)
-    one_post = [post for post in posts if post['id'] == id ]
+    # one_posr = Post.query.get(id)
+    one_post = [post for post in posts if post["id"] == id]
     print(one_post)
     return render_template("feed.html", posts=one_post)
