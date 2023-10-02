4dd2f2c1d6403210514a403a56f7c55b96e10bd2
bradsimpson213 <bradsimpson@icloud.com>
2023-10-02 18:47:36 -0400

    Updated for w17d1

--- week_17/D1/lecture-code/w17d1.py
+++ week_17/D1/lecture-code/w17d1.py
@@ -1,156 +1,146 @@
-# FUNCTIONS
-# def get_average(num_1, num_2):
-#     """This function will accept 2 numbers and return 
-#     their average"""
-#     average = (num_1 + num_2) / 2
-#     print(average)
-#     return average
+def get_average(num_1, num_2):
+    """this function will take in 2 number and return their average"""
+    # This is a comment
+    average = (num_1 + num_2) / 2
+    print(average)
+    return average
 
-# ran_average = get_average(2, 4)
+
+# ran_average = get_average(8, 10)
 # print(ran_average)
+
 # help(get_average)
 
-# STRINGS
-# lunch = "Pizza"
+# STRING
+# lunch = "pizza"
 # print(lunch)
-# print(len(lunch))
-
-# print('Pizza')
-# print("Pizza")
-# print("Let's")
+# print("pizza")
+# print('pizza')
+# print("Let's ")
 # print('Let\'s')
-# print(""" THIS IS 
-# a multi line string
-# where spaces
-# and 
-# new lines
-# are preserved
+# print(""" This is a multi
+# line string where spaces and 
+# new lines get 
+# preserved
 # """)
 
-a=1
-b=2
+
+a = 1
+b = 2
 # print(a)
-# a = "Pizza"
+# a = "wings"
 # print(a)
 
-# LUNCH_FOOD = "Pizza"
+# LUNCH = "pizza"
 
-# print(LUNCH_FOOD)
-# LUNCH_FOOD = "Wings"
-# print(LUNCH_FOOD)
-# print(f"Let's add {a} and {b} together to get {a + b}")
-# print("Let's add {1} and {0} together to get {2}".format(a, b, a + b))
-# print("Let's add {thing2} and {thing1} together to get {thing3}".format(thing1=2, thing2=3, thing3=3))
+# LUNCH = 3
 
-# print('Pizza ' + "is " + "so yummy!")
-# print("Pizza" * 3)
+# print(LUNCH)
 
-sentence = "Brad is very much obsessed with pizza today!"
+# print(f"Let's add {a} and {b} together to get {a+b}!")
+# print(f"{'{'}")
+# print("Let's add {} and {} together to get {}".format(a, b, a+b))
+# print("Let's add {1} and {0} together to get {2}".format(a, b, a+b))
+# print("Let's add {thing_1} and {thing_3} together to get {thing_2}".format(thing_1=a, thing_2=a+b, thing_3=b))
+
+
+# print("Pizza" + " Pizza")
+# print("Pizza " * 3)
+
+# sentence = "Brad is very much obsessed with pizza today"
 
 # print(sentence[2])
-# print(sentence[-3])
-# print(sentence[start:stop:step])
-# print(sentence[:5])
-# print(sentence[1:-2])
+# print(sentence[-1])
+# # print(sentence[start:stop:step])
+# print(sentence[4::2])
 # print(sentence[::-1])
 # print(sentence.split("a"))
 # print(list(sentence))
+# names = ["Brad", "David", "Andrew"]
+# join_str = ", "
+# print(join_str.join(names) )
 
-# names = ['Dan', "Brad", "Andrew", 'David']
-# join_str= ", "
-# print(join_str.join(names))
 
 # BOOLEANS
-a = True
-b = False
+# a = True
+# b = False
 
 # && || !
-# and or not 
-# print(True and False)
-# print(True or False)
-# print(False and True)
-# print(False and False)
-# print(not True)
-# print( True + 1 )
-# print( False + 1 )
-# print( True == 1 )
+# and or not
+# # print(True and False)
+# # print(True or False)
+# # print(not True)
+# # print(False and False)
+# print(1 == 2)
+# print(1 != 2)
+
+# print(int("4") == 4)
+# print(True + 1)
+# print(False + 1)
+# print(1 == True)
+# print(0 == False)
 # print(1 is True)
-# # != 
-# ==
-# print(1==2)
-# print(1!=2)
-# print(1_000_000_000 == 1000000000)
-# print(1_000_000_000)
+# print(0 is False)
 
 
-# NUMBERS 
-# result = 2.0 * 4
+# NUMBERS
+# result = 2. * 4
 # print(result)
-# print(int(result))
-# other_result = 4 / 2
-# print(other_result)
-# yet_another_result = 5 // 2
-# print(yet_another_result)
-# print(int('4'))
-# print(str(4))
-# print(float(5))
+# result_2 = 2 / 4
+# print(result_2)
+# result_3 = 9 // 2
+# print(result_3)
+# print(int(3.5))
+# print(float(6))
+# print(int("7"))
+# # print(int("a"))
+# print(str(3.4))
 
-# Operators
-# + - * / % ** //
+# # + - * / % // **
 
 # print(4**2)
 
-# def long_division(dividend, divisor):
-#     """Takes in 2 numbers and return their result and remainder"""
+# def long_divison(dividend, divisor):
+#     """takes in 2 integers and returns thier quotient and remainder"""
 #     quotient = dividend // divisor
 #     remainder = dividend % divisor
 #     print(quotient, remainder)
-#     return f"{dividend} divided by {divisor} is {quotient} remainder {remainder}"
+#     return f'{dividend} divided by {divisor} is {quotient} remainder {remainder}'
 
 
-# print(long_division(25, 3))
+# print(long_divison(23, 3))
 
 
-# VARIABLES
+# brads_age = 45
+# brads_age += 1
 
-# num_1 = 40
-# print(num_1)
-# num_1 += 1
-# print(num_1)
+# BRADS_AGE = 45 
 
-# num_1 = "Tacos"
-# print(num_1)
+# bags = cases = boxes = cartons = storage_devices = 50
+# print(bags, cases, boxes, cases, cartons, storage_devices)
 
-# bags = cases = 50
-# print(bags, cases)
 
-# DESSERT = "ice cream"
+# IDENTITY vs EQUALITY
+#     is        ==
 
-# Identity vs equality
-#   is     vs  ==
+# my_int = 4
+# my_float = 4.0
 
-my_int = 4
-my_float = 4.0
-
-# print(my_float == my_int)
-# print(my_float is my_int)
-
+# print(my_int == my_float)
+# print(my_int is my_float)
 
 # print("2" == '2')
-# print('2' is "2")
+# print("2" is "2")
 
-# a = None
-# print(id(a))
-# b = "pizza"
-# print(id(b))
-# c = None 
-# print(id(c))
+a = None
+print(id(a))
+b = "pizza"
+print(id(b))
+c = None
+print(id(c))
 d = True
 print(id(d))
 e = 1
 print(id(e))
-f = True
-print(id(f))
-print(e == f)
-print(e is f)
-print(d is f)
+print(a is c)
+print(d == e)
