d648af529617d5973d989c82c3938f0950da9db7
bradsimpson213 <bradsimpson@icloud.com>
2024-01-03 20:00:54 -0500

    updated for EOD importing lecture and guessing game

--- week_17/D2/EOD/guessing.py
+++ week_17/D2/EOD/guessing.py
@@ -1,41 +1,40 @@
 # GUESSING GAME
 
 # Things we will need...
-# 1. random - generate random numbers - x
+# 1. random - generate random numbers - X
 # 2. user input - x
 # 3. iterating - x
 # 4. guess tracker - number of guesses - x
-# 5. conditionals - x 
+# 5. conditionals - x
 # 6. helper function?
 from random import randint
-
 
 
 def guess(start, end):
     bad_guess = True
 
     while bad_guess:
-
         try:
-            user_guess = int(input(f"Pick a number between {start} and {end}: "))
-
+            user_input = int(input(f"Pick a number from {start} to {end}: "))
+        
         except ValueError:
-            print("We need to enter number values for the guessing game... ")
+            print("We need to enter numbers for the guessing game, please try again!")
             continue
 
         else:
-            if user_guess < start or user_guess > end:
-                print(f"Numbers need to be between {start} and {end}, not {user_guess}")
+            if user_input < start or user_input > end:
+                print(f"Numbers need to between {start} to {end}, not {user_input}")
                 continue
 
         bad_guess = False
-    return user_guess
+        return user_input
 
+        
 
 
 
 def guessing_game():
-    """its the guessing game, duh"""
+    """its the guessing game, duh!"""
     print('Welcome to the guessing game!')
     start = 1
     end = 20
@@ -43,25 +42,23 @@ def guessing_game():
     guesses = 5
 
     while guesses > 0:
-        # user_guess = int(input(f"Pick a number between {start} and {end}: "))
-        user_guess = guess(start, end)
-        guesses -= 1 
+        # user_input = int(input(f"Pick a number from {start} to {end}: "))
+        user_input = guess(start, end)
+        guesses -= 1
 
-        if user_guess == winning_number:
-            print(f"You guessed it! The number was {winning_number}!  You used { 5 - guesses } guesses!")
-            # implement play agaun
+        if user_input == winning_number:
+            print(f"YOU GUESSED IT!!! The number was {winning_number}.  You used {5 - guesses} guesses")
+            # play again functionality
             return
 
-        elif user_guess > winning_number:
-            print(f"You guessed too high! Try again!  You have {guesses} guesses left...")
-            continue
+        elif user_input > winning_number:
+            print(f"You guessed too high!  Try again! You have {guesses} guesses left")
 
         else:
-            print(f"You guessed too low! Try again!  You have {guesses} guesses left...")
-            continue
+            print(f"You guessed too low!  Try again! You have {guesses} guesses left")
 
-    print(f"You are out of guesses, the number was { winning_number }")
 
+   
 guessing_game()
 
 
---
+++ week_17/D2/EOD/helpers.py
@@ -0,0 +1,18 @@
+
+
+
+def add2(num):
+    return num + 2
+
+
+
+def add4(num):
+    return num + 4
+
+
+
+names = ["Brad", 'David']
+
+
+class Tomato:
+    pass
